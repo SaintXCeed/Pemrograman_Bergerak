5d1baef8f15449ab983147904f033a5fa6aab41d
Hadistya <119101368+SaintXCeed@users.noreply.github.com>
2026-01-15 10:18:42 +0700

    update bab 7

---
+++ bab7/app/src/main/java/com/example/sqlitedemo/db/Student.kt
@@ -0,0 +1,12 @@
+package com.example.sqlitedemo.db
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "students")
+data class Student(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val age: Int
+)
